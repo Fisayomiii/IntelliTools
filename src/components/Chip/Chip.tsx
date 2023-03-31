88ff589668f9aeb3fe302bc1ef0b0c63d5bc5bb7
oluwafisayomi <wrld26993@gmail.com>
2023-03-31 15:47:42 +0100

    [Feat] added filter animations

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,6 +1,7 @@
 import Card from "../Cards/Card";
-import { useState } from 'react'
-import AitoolsData from '../../data/Tools.json'
+import { useState } from 'react';
+import AitoolsData from '../../data/Tools.json';
+import { motion, AnimatePresence } from "framer-motion";
 
 function Chip() {
     const categoryData: string[] = ["All", "Generative Art", "AI Detectors", "Chatbots", "Music", "Writing", "Finance", "Fun", "Food"];
@@ -23,7 +24,7 @@ function Chip() {
                 <div className="max-w-xl w-full mx-auto min-w-0 dark:highlight-white/5">
                     <div className="overflow-x-auto flex">
                         {categoryData.map((tab: string) => {
-                            return <div className="cursor-pointer flex justify-center items-center m-1 font-medium py-1 px-10 text-center rounded-lg text-slate-900 border border-slate-900 dark:text-slate-50 dark:border-zinc-500 hover:bg-zinc-100 hover:dark:bg-zinc-800" key={tab} onClick={() => handleCategory(tab)}>
+                            return <div className="cursor-pointer flex justify-center items-center m-1 font-medium py-0 px-8 text-center rounded-lg text-slate-900 border border-slate-900 dark:text-slate-50 dark:border-zinc-500 hover:bg-zinc-100 hover:dark:bg-zinc-800" key={tab} onClick={() => handleCategory(tab)}>
                                 <div className=" text-base font-semibold leading-7 shadow-sm ">
                                     {tab}
                                 </div>
@@ -34,11 +35,13 @@ function Chip() {
             </div>
 
             <div className="min-h-screen flex justify-center items-center py-20">
-                <div className="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
-                    {onDisplay.map((tool) => {
-                        return <Card tool={tool} key={tool.image} />
-                    })}
-                </div>
+                <motion.div layout className="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
+                    <AnimatePresence>
+                        {onDisplay.map((tool) => {
+                            return <Card tool={tool} key={tool.image} />
+                        })}
+                    </AnimatePresence>
+                </motion.div>
             </div>
         </>
     )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import Logo from "../../assets/Images/text.png";
 import DarkModeToggle from "../../darkmode/Darkmode";
+import 'animate.css';
 
 function Header() {
   return (
     <>
-      <header className="backdrop-filter backdrop-blur-lg fixed top-7 left-4 right-4 sm:left-64 sm:right-64 z-50 rounded-lg shadow-lg transition-all border border-slate-500 hover:border-slate-600">
-        <nav className="flex justify-between items-center py-3 px-6">
+      <header className="backdrop-filter backdrop-blur-lg fixed top-4 left-4 right-4 sm:left-64 sm:right-64 z-50 rounded-lg shadow-lg transition-all border border-slate-500 hover:border-slate-600">
+        <nav className="flex justify-between items-center py-2 px-6">
           <div className="flex items-center" title="Intellitools">
             <a href="#" className="text-gray-900 font-medium text-lg mr-8">
               <img src={Logo} alt="Logo" className="h-12" />
@@ -13,17 +14,17 @@ function Header() {
           </div>
           <div className="flex items-center justify-center">
 
-            <button className="text-gray-900 font-medium rounded-full">
+            <button className="animate__animated animate__fadeInUp text-gray-900 font-medium rounded-full">
               <DarkModeToggle />
             </button>
 
-            <button className="text-gray-900 font-medium py-2 px-4 rounded-full" title="Star on Github 🙏">
+            <button className="animate__animated animate__fadeInUp animate__delay-1s text-gray-900 font-medium py-2 px-4 rounded-full" title="Star on Github 🙏">
               <a href="https://github.com/Fisayomiii/Intellitools" target="_blank" rel="noreferrer" className="block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
                 <i className="bx bxl-github bx-sm"></i>
               </a>
             </button>
 
-            <button className="text-gray-900 font-medium rounded-full" title="Give me a follow">
+            <button className="animate__animated animate__fadeInUp animate__delay-2s text-gray-900 font-medium rounded-full" title="Give me a follow">
               <a href="https://twitter.com/999tweets_her" target="_blank" rel="noreferrer" className="block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
                 <i className="bx bxl-twitter bx-sm"></i>
               </a>
